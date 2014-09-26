a7ae076bca4e7bbcab63c70ed27a45691848e6b2
alwxndr <alwxndr@gmail.com>
2014-09-26 22:06:48 +0400

    small improvements to schema

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject eshemozhno "0.1.0-SNAPSHOT"
   :description "An app for telling if you still can buy alcoholic beverages in a distant corner of Mother Russia."
+  :source-paths ["src/clj"]
   :url "http://github.com/si14/eshemozhno"
   :license {:name "CC0"
             :url "https://creativecommons.org/publicdomain/zero/1.0/legalcode"}
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-figwheel "0.1.4-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2356"]
 
-                 [compojure "1.1.9"]
-                 [ring "1.3.1"]
                  [prismatic/plumbing "0.3.3"]
                  [prismatic/fnhouse "0.1.1"]
                  [clj-http "0.9.0"]
@@ -23,14 +24,7 @@
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
 
-
   :jvm-opts ["-Xmx512M"]
-  :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-figwheel "0.1.4-SNAPSHOT"]]
-  :figwheel {
-    :http-server-root "public"
-    :port 3449
-    :css-dirs ["resources/public/css"]}
   :cljsbuild {
     :builds [{:id "dev"
               :source-paths ["src/cljs/eshemozhno" "src/cljs/figwheel"]
@@ -38,4 +32,8 @@
                 :output-to "resources/public/js/eshemozhno.js"
                 :output-dir "resources/public/js/out"
                 :optimizations :none
-                :source-map true}}]})
+                :source-map true}}]}
+  :figwheel {
+    :http-server-root "public"
+    :port 3449
+    :css-dirs ["resources/public/css"]})
